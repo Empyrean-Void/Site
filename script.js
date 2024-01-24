fc6f05a48e8499e7ed30097a5e5c7a4db0b4dba9
Empyrean-Void <jshortz@proton.me>
2024-01-23 22:26:26 -0700

    Added fonts

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.getElementById('hamburger-menu');
-    const menu = document.getElementById('menu');
-    const links = document.querySelectorAll('.header-nav ul li a');
+    const menuButton = document.getElementById('site-menu');
+    const menu = document.querySelector('.header-nav');
     const themeToggle = document.getElementById('theme-toggle');
 
     // Toggle the hamburger menu
     const toggleMenu = () => {
-        menu.parentElement.classList.toggle('transition');
-        menu.parentElement.style.display = menu.parentElement.style.display === 'block' ? 'none' : 'block';
+        menu.classList.toggle('is-active');
     };
 
-    hamburger.addEventListener('click', toggleMenu);
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            if (window.innerWidth < 600) {
-                toggleMenu();
-            }
-        });
-    });
+    menuButton.addEventListener('click', toggleMenu);
 
     // Update the theme and save to local storage
     const updateTheme = () => {
         const isDarkTheme = document.body.classList.toggle('dark-theme');
+        themeToggle.textContent = isDarkTheme ? 'sunny' : 'dark_mode';
         localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
     };
 
     if (themeToggle) {
-        themeToggle.addEventListener('change', updateTheme);
+        themeToggle.addEventListener('click', updateTheme);
     }
 
     // Set the initial theme state from local storage
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
         document.body.classList.toggle('dark-theme', savedTheme === 'dark');
-        if (themeToggle) {
-            themeToggle.checked = savedTheme === 'dark';
-        }
+        themeToggle.textContent = savedTheme === 'dark' ? 'sunny' : 'dark_mode';
     }
 });
